69d6ea7699ba2514ae7d0c95bcbed150f0cdc0e7
Anshuman Patek <78948391+AnshumanPatek@users.noreply.github.com>
2024-04-10 23:07:28 +0530

    this simple effect data fetching

---
+++ Effectfecthing Data/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import DataFetching from './Components/DataFetching'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <DataFetching/>
+      
+    </>
+  )
+}
+
+export default App
