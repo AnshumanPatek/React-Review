c5c25da034fbec53e4950f7151489128fd9fb5bf
Anshuman Patek <78948391+AnshumanPatek@users.noreply.github.com>
2024-04-23 11:46:20 +0530

    Bg changer
    full projects

---
+++ Bg Changer/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function App() {
+  const [Color, SetColor] = useState("Black");
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: Color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => SetColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+
+
+
+          <button
+          onClick={() => SetColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "green"}}
+          >Green</button>
+          <button
+          onClick={() => SetColor("blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "blue"}}
+          >Blue</button>
+
+          <button onClick={()=>SetColor("yellow")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor:"yellow"}}>
+          yellow
+            
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
